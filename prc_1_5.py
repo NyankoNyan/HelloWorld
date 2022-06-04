f54fa7ffbb979dc113dde7303d449000ca277d55
NN <31081277+NyankoNyan@users.noreply.github.com>
2022-06-04 20:33:18 +0300

    Update prc_1_5.py

--- prc_1_5.py
+++ prc_1_5.py
@@ -1,15 +1,31 @@
-s = "45678998765"
+
+
+s = "45687658658769486378998765"
 print(s)
 
 # print(s.count("4"))
 #
 # print(set(s))
 
-counts = {c:s.count(c) for c in set(s)}
+counts = {c: s.count(c) for c in set(s)}
 print(counts)
 #
 # print(counts.get("5"))
 # print(counts["5"])
 
+# f = counts.get
+#
+# print(f("5"))
+
+
+
 biggest_3 = sorted(counts, key=counts.get, reverse=True)[:3]
+# biggest_3 = sorted(counts, key=counts.get, reverse=True)
+
+# def my_sort(x):
+#     return -counts[x]
+# biggest_3 = sorted(counts, key=my_sort)
+#
+# biggest_3 = sorted(counts, key=lambda x:-counts[x])
+
 print(biggest_3)
